a1fa5e5950101f5acc90284a0849c91c26edcb6c
Tim Leonard <tim@programmingbytim.com>
2026-07-30 15:29:58 +0100

    Allow using enterprise VS on github runners

--- projects/scripts/msvc_x64_compile_and_run_tests.bat
+++ projects/scripts/msvc_x64_compile_and_run_tests.bat
@@ -9,7 +9,11 @@ set SOURCENAME="test"
 
 cd "%BINDIR%"
 
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat" -arch=x86
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\" (
+    CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=x64
+) else (
+    CALL "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat" -arch=x64
+)
 
 cl.exe "%SRCDIR%\%SOURCENAME%.c" /std:c11 /link /out:"%BINDIR%\%SOURCENAME%.exe"
 
