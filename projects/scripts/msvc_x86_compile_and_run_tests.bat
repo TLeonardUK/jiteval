9861880b3d6026c308453b7cfff696611eea1f46
Tim Leonard <tim@programmingbytim.com>
2026-07-30 15:26:08 +0100

    First setup of CI

---
+++ projects/scripts/msvc_x86_compile_and_run_tests.bat
@@ -0,0 +1,18 @@
+@echo OFF
+
+git fetch
+git pull
+
+set BINDIR=%cd%\..\..\bin\
+set SRCDIR=%cd%\..\..\
+set SOURCENAME="test"
+
+cd "%BINDIR%"
+
+CALL "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat" -arch=x64
+
+cl.exe "%SRCDIR%\%SOURCENAME%.c" /std:c11 /link /out:"%BINDIR%\%SOURCENAME%.exe"
+
+"%BINDIR%\%SOURCENAME%.exe"
+
+exit /B %errorlevel%
