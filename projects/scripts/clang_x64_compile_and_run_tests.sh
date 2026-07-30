5a664bcf78660c384286472673176203a2b9d569
Tim Leonard <tim@programmingbytim.com>
2026-07-30 16:03:34 +0100

    Clang CI

---
+++ projects/scripts/clang_x64_compile_and_run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+git fetch
+git pull
+
+mkdir -p ../../bin/
+clang ../../test.c -lm -O3 -m64 -o ../../bin/test.elf
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo Compilation Failed
+    exit 1
+fi
+
+../../bin/test.elf
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo Tests failed
+    exit 1
+fi
+
+echo Completed Successfully
+exit 0
